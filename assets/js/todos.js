5d78a86da38814caa40467f9f9d890b9db27ad63
Deepali2 <hdgarg@gmail.com>
2018-10-30 13:13:26 -0700

    creates todos with event listeners

---
+++ assets/js/todos.js
@@ -0,0 +1,18 @@
+$("ul").on('click', 'li', function() {
+  $(this).toggleClass("completed");
+})
+
+$("ul").on('click', 'span', function(event) {
+  event.stopPropagation();   //to stop event bubbling
+  $(this).parent().fadeOut(500, function() { //to make sure remove is called only after the fadeout is complete
+    $(this).remove();
+  });  
+})
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    let todoText = $(this).val();
+    $(this).val('');
+    $("ul").append(`<li><span>X </span>${todoText}</li>`);
+  }
+})
